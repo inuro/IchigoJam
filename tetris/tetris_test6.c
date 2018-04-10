fb2f65cf44b20358ae323f2a567252034a71dcd6
inuro <inuro@infofreako.com>
2018-04-09 18:42:21 -0700

    Update tetris_test6.c

--- tetris/tetris_test6.c
+++ tetris/tetris_test6.c
@@ -13,8 +13,8 @@
 
 //store patterns on BASIC array( [0]- 
 
-#define VRAM_OFFSET   0x900 //VRAM先頭仮想アドレス
-#define VARS_OFFSET   0x800 //変数領域先頭仮想アドレス
+#define VRAM_OFFSET   0x900 //VRAM offset
+#define VARS_OFFSET   0x800 //Variables offset
 
 //val must be: tetromino_type<<2 + rotate_pattern
 uint16_t usr_calc(uint16_t val, uint8_t* mem) {
