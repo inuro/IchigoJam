d0ad192d953bd6eabb22556e7afdc2d094ba77e6
inuro <inuro@infofreako.com>
2018-04-14 00:43:34 -0700

    Update tetris_core_mod.s

--- tetris/tetris_core_mod.s
+++ tetris/tetris_core_mod.s
@@ -29,52 +29,49 @@ usr_calc:
 	ldrb	r3, [r2,0xc]	@load byte from 0x8fc(Y) to R3
 	str		r3, [sp, #4]	@store R3=Y to SP+4 = R5??
 	ldrb	r3, [r2,0x6]	@load_byte from 0x8f6(V=B) to R3
-	ldrb	r2, [r2,0x8]	@load_byte from 0x8f8(W=R) to R2
+	ldrb	r4, [r2,0x8]	@load_byte from 0x8f8(W=R) to R4
 	lsls	r3, r3, #2		@R3<<2
-	adds	r3, r3, r2		@R3+=R2 -> R3 = B<<2 + R = BLOCK
-	movs	r2, #129		@R2 = 129 (0x81)
-	lsls	r3, r3, #1		@R3<<1 -> BLOCK<<1
-	lsls	r2, r2, #4		@R2<<4 = 0x810 (ARRAY_OFFSET)
-	adds	r3, r1, r3
-	adds	r3, r3, r2
-	ldrh	r5, [r3]
-	movs	r2, #0
-	movs	r3, #3
-	movs	r4, r2
-	mov	ip, r3
+	adds	r3, r3, r4		@R3+=R4 -> R3 = B<<2 + R = BLOCK
+	lsls	r3, r3, #1		@R3<<1 -> BLOCK*2(word offset)
+    subs    r2, 0xe0        @shift R2 from 0x8f0 to 0x810
+	ldrh	r5, [r2,r3]     @load r5=block
+	movs	r2, #0          @R2=0 -> shift value
+	movs	r3, #3          @R3=3
+	movs	r4, r2          @R4=0 -> collision flag
+	mov	ip, r3              @IP=3
 .L3:
-	movs	r3, r5
-	mov	r6, ip
-	asrs	r3, r3, r2
-	ands	r3, r6
-	ldr	r6, [sp, #4]
-	adds	r7, r2, #2
-	adds	r3, r3, r6
-	movs	r6, #144
-	lsls	r3, r3, #5
-	lsls	r6, r6, #4
-	adds	r3, r3, r6
-	movs	r6, r5
-	asrs	r6, r6, r7
-	movs	r7, r6
-	mov	r6, ip
-	ands	r7, r6
-	ldr	r6, [sp]
-	adds	r7, r7, r6
-	adds	r3, r3, r7
-	adds	r3, r1, r3
-	ldrb	r7, [r3]
-	subs	r6, r7, #1
-	sbcs	r7, r7, r6
-	orrs	r4, r7
-	cmp	r0, #0
-	blt	.L2
-	strb	r0, [r3]
+	movs	r3, r5          @R3=block
+	mov	r6, ip              @R6=3
+	asrs	r3, r3, r2      @block >> (i*4)
+	ands	r3, r6          @R3 = b = (block >> (i*4)) & 3
+	ldr	r6, [sp, #4]        @R6=Y
+	adds	r7, r2, #2      @R7=(i*4)+2
+	adds	r3, r3, r6      @R3 = (y + b)
+	movs	r6, #144        @R6=0x90
+	lsls	r3, r3, #5      @R3 = (y + b)*32
+	lsls	r6, r6, #4      @R6=0x900
+	adds	r3, r3, r6      @R6=0x900 + (y+b)*32
+	movs	r6, r5          @R6=block
+	asrs	r6, r6, r7      @block >> ((i*4)+2)
+	movs	r7, r6          @R7 = (block >> ((i*4)+2))
+	mov	r6, ip              @R6=3
+	ands	r7, r6          @R7 = a = (block >> ((i*4)+2)) & 3
+	ldr	r6, [sp]            @R6=X
+	adds	r7, r7, r6      @R7 = (x + a)
+	adds	r3, r3, r7      @R3 = 0x900 + (x + a) + (y+b)*32
+	adds	r3, r1, r3      @R3 += memory offset
+	ldrb	r7, [r3]        @R7 = vram value = [R3]
+	subs	r6, r7, #1      @R6=R7-1
+	sbcs	r7, r7, r6      @R7=R7-R6 these 2 lines means (R7 != 0)
+	orrs	r4, r7          @R4 |= R7
+	cmp	r0, #0              @if R0 <= 0
+	blt	.L2                 @   goto .L2
+	strb	r0, [r3]        @Write R0 to vram[R3]
 .L2:
-	adds	r2, r2, #4
-	cmp	r2, #16
-	bne	.L3
-	sxth	r0, r4
+	adds	r2, r2, #4      @R2 = R2 + 4
+	cmp	r2, #16             @if R2 <> 16 (means i <> 4)
+	bne	.L3                 @   goto .L3
+	sxth	r0, r4          @R0=(int16_t)collision
 	@ sp needed
 	pop	{r1, r2, r3, r4, r5, r6, r7, pc}
 .L6:
